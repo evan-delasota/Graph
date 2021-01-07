da099355cb06fe3213ea72283453a80ad9bb710c
Evan D <Evan_delasota@yahoo.com>
2021-01-07 02:53:00 +0000

    Minor changes

--- Main.java
+++ Main.java
@@ -6,14 +6,11 @@ class Main {
     graph.addNode("Alice");
     graph.addNode("Mark");
     graph.addNode("Rob");
-    graph.addNode("Maria");
 
     graph.addEdge("Bob", "Alice");
     graph.addEdge("Bob", "Rob");
     graph.addEdge("Alice", "Mark");
     graph.addEdge("Rob", "Mark");
-    graph.addEdge("Alice", "Maria");
-    graph.addEdge("Rob", "Maria");
 
     System.out.print(graph.displayGraph());
   }
